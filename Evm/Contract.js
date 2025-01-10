72dbd77293e87ba50af82b0517a7c9b257b8a9d0
haolifeng <haolifengwang@126.com>
2025-01-10 11:29:51 +0800

    update

--- Evm/Contract.js
+++ Evm/Contract.js
@@ -1,1 +1,10 @@
 const { ethers } = require("ethers");
+
+class EvmContract {
+    constructor(url, scAddr, abi) {
+        this.provider = new ethers.providers.JsonRpcProvider(url);
+        this.scInst = new ethers.Contract(scAddr, abi);
+    }
+}
+
+module.exports = EvmContract;
